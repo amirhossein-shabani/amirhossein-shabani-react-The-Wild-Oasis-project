f369300b2b392dcfbc87d2c761d82e9e460bfc1f
amsein <shabanishabaniamirhossein98@gmail.com>
2026-09-06 18:36:08 +0330

    fix some issue

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useRecentBookings } from "./useREcentBookings";
+import { useRecentBookings } from "./useRecentBookings";
 import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
